fbd4c3c323bb31e83c4cfd7b403b282fcabf71f8
Sanix Darker <sanix000@yahoo.fr>
2017-09-18 19:25:14 +0200

    Simple refresh

---
+++ Ki.js
@@ -0,0 +1,12 @@
+
+// Still working it
+const express = require('express')
+const app = express()
+
+app.get('/', function (req, res) {
+  res.send('Ki!')
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
